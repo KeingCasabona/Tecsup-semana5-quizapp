e3ee04cff6dc970eedba2ae972c803ad6eed97fb
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-07-13 01:22:01 -0500

    feat: Add base1 interfaz

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,77 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
+      home: QuizPage(),
+    );
+  }
+}
+
+class QuizPage extends StatelessWidget {
+  const QuizPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 50, 20, 119),
+      appBar: AppBar(
+        title: const Text(
+          'QuizApp',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        backgroundColor: Color.fromARGB(255, 94, 5, 152),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 8,
+              child: Center(
+                child: Text(
+                  '¿El hombre llego a la luna? hhhhlhl hlkhklh hlhlh loh lhl  ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  'Verdadero',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
+                color: Colors.green,
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  'Falso',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
+                color: Colors.red,
+              ),
+            ),
+            SizedBox(height: 80),
+          ],
         ),
       ),
     );
